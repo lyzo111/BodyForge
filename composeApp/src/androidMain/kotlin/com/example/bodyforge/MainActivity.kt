39e3d2fda33890064c4d5594d8695847144addfd
lyzo111 <chrisus999@gmail.com>
2025-06-15 23:34:22 +0200

    moved mainactivity, removed one redundant file

--- composeApp/src/androidMain/kotlin/com/example/bodyforge/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/example/bodyforge/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.bodyforge
 
-import com.example.composeApp.App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.bodyforge.di.AppContainer
+import com.bodyforge.data.local.DatabaseDriverFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Initialize DI - Simple approach
+        val databaseDriverFactory = DatabaseDriverFactory(this)
+        AppContainer.init(databaseDriverFactory)
 
         setContent {
             App()
